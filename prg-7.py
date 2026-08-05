23dfa11ef945d3f80a2b1f8c8c00626715ec6942
shanmukh <karanamvenkatasaishanmukh@gmail.com>
2026-08-05 10:56:04 +0530

    Add files via upload

---
+++ prg-7.py
@@ -0,0 +1,20 @@
+from nltk import word_tokenize, pos_tag
+s = "The professor explained the algorithm clearly."
+t = pos_tag(word_tokenize(s))
+noun = []
+verb = []
+adjective = []
+adverb = []
+for w, p in t:
+    if p.startswith("NN"):
+        noun.append(w)
+    elif p.startswith("VB"):
+        verb.append(w)
+    elif p.startswith("JJ"):
+        adjective.append(w)
+    elif p.startswith("RB"):
+        adverb.append(w)
+print("Nouns:", noun)
+print("Verbs:", verb)
+print("Adjectives:", adjective)
+print("Adverbs:", adverb)
